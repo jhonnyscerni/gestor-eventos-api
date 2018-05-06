9ed0a47b55a5c85cb62bce9952ea19080edce13b
jhonnyscerni <jhonnyscerni@gmail.com>
2018-05-05 21:32:11 -0300

    End Point Geracao de QrCode e Envio de Email

--- src/main/java/br/jus/tre_pa/seven/rest/InscricaoRest.java
+++ src/main/java/br/jus/tre_pa/seven/rest/InscricaoRest.java
@@ -1,9 +1,14 @@
 package br.jus.tre_pa.seven.rest;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +18,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.jus.tre_pa.seven.domain.Inscricao;
+import br.jus.tre_pa.seven.mail.Mailer;
 import br.jus.tre_pa.seven.repository.InscricaoRepository;
+import net.glxn.qrgen.javase.QRCode;
 
 @RestController
 @RequestMapping("/inscricoes")
@@ -21,6 +28,9 @@ public class InscricaoRest {
 	
 	@Autowired
 	private InscricaoRepository inscricaoRepository;
+	
+	@Autowired
+	private Mailer mailer;
 	
 	@GetMapping
 	public List<Inscricao> listar() {
@@ -44,6 +54,31 @@ public class InscricaoRest {
 		Inscricao inscricao = inscricaoRepository.findOneParticipanteByInscricao(idInscricao);
 		return inscricao != null ? ResponseEntity.ok(inscricao) : ResponseEntity.notFound().build();
 	}
+	
+	@GetMapping("gerar-qrcode/{id}")
+	public ResponseEntity<byte[]> qr(@PathVariable Long id) {
+
+		Inscricao inscricao = inscricaoRepository.findOne(id);
+		String template = "mail/evento";
+			
+		byte[] bytes = QRCode.from(inscricao.getCodigoQrCode().toString()).withSize(120, 120).stream()
+				.toByteArray();
+		final HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_PNG);
+		headers.setContentLength(bytes.length);
+		
+		Map<String, Object> variaveis = new HashMap<>();
+		variaveis.put("inscricao", inscricao);
+		variaveis.put("qrcoe", headers);
+
+		mailer.enviarEmail("jhonnyscerni", Arrays.asList("siberiusapp@gmail.com"), "TESTE", template, variaveis);
+		
+		return new ResponseEntity<byte[]>(bytes, headers, HttpStatus.CREATED);
+		
+
+	}
+
+
 	
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
