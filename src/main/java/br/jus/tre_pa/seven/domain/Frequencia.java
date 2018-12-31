438538ebc38aa3df4fb6d6ce2187a51d9b45f1fe
jhonnyscerni <jhonnyscerni@gmail.com>
2018-12-31 00:00:40 -0300

    Ajustes

--- src/main/java/br/jus/tre_pa/seven/domain/Frequencia.java
+++ src/main/java/br/jus/tre_pa/seven/domain/Frequencia.java
@@ -23,7 +23,7 @@ public class Frequencia implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column
